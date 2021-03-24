87e8c0769e6e2040fcdb6784cc2eff26d5cd3d49
oldmanmuffins <mluper.official@protonmail.com>
2021-03-24 14:41:10 -0400

    electron-build setup

--- src/electron/index.js
+++ src/electron/index.js
@@ -3,5 +3,6 @@ const { app, BrowserWindow } = require('electron');
 app.on('ready', () => {
     const window = new BrowserWindow({ width: 800, height: 600 });
     window.setMenu(null);
-    window.loadFile('../index.html')
+    //window.webContents.openDevTools()
+    window.loadURL(`file://${__dirname}/../index.html`);
 });
